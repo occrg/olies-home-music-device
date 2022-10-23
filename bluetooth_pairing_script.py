f105e222b50770aeb4b17c0301efc42cd5f505ed
Olie Ryan-George <olie@occrg.uk>
2022-10-23 14:10:26 +0100

    replaced manual logging with pexpect logging

--- bluetooth_pairing_script.py
+++ bluetooth_pairing_script.py
@@ -1,6 +1,7 @@
 import pexpect
 import time
 import traceback
+import sys
 
 def allow_bluetooth_connection():
     pexpect_child = initial_setup_commands()
@@ -8,27 +9,21 @@ def allow_bluetooth_connection():
 
 def initial_setup_commands():
     pexpect_child = pexpect.spawn("bluetoothctl")
+    pexpect_child.logfile = sys.stdout.buffer
     pexpect_child.send("power on\n")
-    print("'power on' command run")
     pexpect_child.send("agent on\n")
-    print("'agent on' command run")
     pexpect_child.send("default-agent\n")
-    print("'default-agent' command run")
     pexpect_child.send("discoverable on\n")
-    print("'discoverable on' command run")
     return pexpect_child
 
 def expect_connections(pexpect_child):
     try:
         pexpect_child.expect("Device (([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2}))")
         device_mac = pexpect_child.match.group(1)
-        print("Device " + device_mac.decode("utf-8") + " is attempting to connect.\n")
 
         pexpect_child.expect("Confirm passkey ([\w\W]{6}) \(yes\/no\)")
         passkey = pexpect_child.match.group(1)
-        print("Passkey (" + passkey.decode("utf-8") + ") confirmation requested.\n")
         pexpect_child.send("yes\n")
-        print("Confirmed passkey\n")
 
         service_auth_key(pexpect_child, "one")
         service_auth_key(pexpect_child, "two")
@@ -36,7 +31,6 @@ def expect_connections(pexpect_child):
         time.sleep(1)
 
         pexpect_child.send("trust " + device_mac.decode("utf-8") + "\n")
-        print("Trusted device " + device_mac.decode("utf-8") + "\n")
 
         pexpect_child.close()
 
@@ -53,10 +47,7 @@ def expect_connections(pexpect_child):
 def service_auth_key(pexpect_child, service_key_request_num):
     res_service_key_auth = pexpect_child.expect("Authorize service ([\w\W]{8}-[\w\W]{4}-[\w\W]{4}-[\w\W]{4}-[\w\W]{12}) \(yes\/no\):")
     service_key = pexpect_child.match.group(1)
-    print("Service key " + service_key_request_num +
-        " (" + service_key.decode("utf-8") + ") confirmation requested.\n")
     pexpect_child.send("yes\n")
-    print("Confirmed service key " + service_key_request_num + "\n")
     time.sleep(1)
 
 allow_bluetooth_connection()
